78f0440558742c1aa5e48014b243411c19ffef92
Abdul Rojak <122954244+bigzero55@users.noreply.github.com>
2024-09-10 07:12:56 +0700

    update models/usersModel.js

--- models/usersModel.js
+++ models/usersModel.js
@@ -1,18 +1,16 @@
 const db = require("./db");
 
-// Fungsi untuk menambah user
-const addUser = (username, password, full_name, callback) => {
-  const sql = `
-    INSERT INTO users (username, password, full_name)
-    VALUES (?, ?, ?)
-  `;
+// Tambah user baru
+const addUser = (user, callback) => {
+  const { username, password, full_name } = user;
+  const sql = "INSERT INTO users (username, password, full_name) VALUES (?, ?, ?)";
   const params = [username, password, full_name];
   db.run(sql, params, function (err) {
     callback(err, this.lastID);
   });
 };
 
-// Fungsi untuk mendapatkan semua users
+// Dapatkan semua users
 const getAllUsers = (callback) => {
   const sql = "SELECT * FROM users";
   db.all(sql, [], (err, rows) => {
@@ -20,7 +18,7 @@ const getAllUsers = (callback) => {
   });
 };
 
-// Fungsi untuk mendapatkan user berdasarkan ID
+// Dapatkan user berdasarkan ID
 const getUserById = (id, callback) => {
   const sql = "SELECT * FROM users WHERE id = ?";
   db.get(sql, [id], (err, row) => {
@@ -28,23 +26,20 @@ const getUserById = (id, callback) => {
   });
 };
 
-// Fungsi untuk mengupdate user
-const updateUser = (id, username, password, full_name, callback) => {
-  const sql = `
-    UPDATE users
-    SET username = ?, password = ?, full_name = ?
-    WHERE id = ?
-  `;
+// Update data user
+const updateUser = (id, user, callback) => {
+  const { username, password, full_name } = user;
+  const sql = "UPDATE users SET username = ?, password = ?, full_name = ? WHERE id = ?";
   const params = [username, password, full_name, id];
-  db.run(sql, params, function (err) {
+  db.run(sql, params, (err) => {
     callback(err);
   });
 };
 
-// Fungsi untuk menghapus user
+// Hapus user berdasarkan ID
 const deleteUser = (id, callback) => {
   const sql = "DELETE FROM users WHERE id = ?";
-  db.run(sql, [id], function (err) {
+  db.run(sql, [id], (err) => {
     callback(err);
   });
 };
