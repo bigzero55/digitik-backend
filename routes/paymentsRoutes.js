93926d767143226a61b0b7d91a3f96b1e0c80dc2
Abdul Rojak <122954244+bigzero55@users.noreply.github.com>
2024-09-11 05:12:30 +0700

    update routes/paymentsRoutes.js

--- routes/paymentsRoutes.js
+++ routes/paymentsRoutes.js
@@ -2,19 +2,19 @@ const express = require("express");
 const router = express.Router();
 const paymentsController = require("../controllers/paymentsController");
 
-// Rute untuk menambah pembayaran dengan Midtrans
-router.post("/add", paymentsController.addPayment);
+// Rute untuk menambah pembayaran baru
+router.post("/payments", paymentsController.addPayment);
 
-// Rute untuk mendapatkan semua payments
-router.get("/", paymentsController.getAllPayments);
+// Rute untuk mendapatkan semua pembayaran
+router.get("/payments", paymentsController.getAllPayments);
 
-// Rute untuk mendapatkan payment berdasarkan ID
-router.get("/:id", paymentsController.getPaymentById);
+// Rute untuk mendapatkan pembayaran berdasarkan ID
+router.get("/payments/:id", paymentsController.getPaymentById);
 
-// Rute untuk memperbarui payment
-router.put("/:id", paymentsController.updatePayment);
+// Rute untuk mengupdate pembayaran berdasarkan ID
+router.put("/payments/:id", paymentsController.updatePayment);
 
-// Rute untuk menghapus payment
-router.delete("/:id", paymentsController.deletePayment);
+// Rute untuk menghapus pembayaran berdasarkan ID
+router.delete("/payments/:id", paymentsController.deletePayment);
 
 module.exports = router;
