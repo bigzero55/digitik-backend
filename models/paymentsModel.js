30a97e8c17661d9c40f884b131e68e1fc825f429
Abdul Rojak <122954244+bigzero55@users.noreply.github.com>
2024-09-10 07:17:14 +0700

    update models/paymentsModel.js

--- models/paymentsModel.js
+++ models/paymentsModel.js
@@ -1,18 +1,19 @@
 const db = require("./db");
 
-// Fungsi untuk menambah payment
-const addPayment = (reservationId, amount, status, paidAt, callback) => {
+// Tambah pembayaran baru
+const addPayment = (payment, callback) => {
+  const { reservation_id, amount, status, paid_at } = payment;
   const sql = `
     INSERT INTO payments (reservation_id, amount, status, paid_at)
     VALUES (?, ?, ?, ?)
   `;
-  const params = [reservationId, amount, status, paidAt];
+  const params = [reservation_id, amount, status, paid_at];
   db.run(sql, params, function (err) {
     callback(err, this.lastID); // Mengembalikan ID pembayaran yang baru
   });
 };
 
-// Fungsi untuk mendapatkan semua payments
+// Dapatkan semua pembayaran
 const getAllPayments = (callback) => {
   const sql = "SELECT * FROM payments";
   db.all(sql, [], (err, rows) => {
@@ -20,7 +21,7 @@ const getAllPayments = (callback) => {
   });
 };
 
-// Fungsi untuk mendapatkan payment berdasarkan ID
+// Dapatkan pembayaran berdasarkan ID
 const getPaymentById = (id, callback) => {
   const sql = "SELECT * FROM payments WHERE id = ?";
   db.get(sql, [id], (err, row) => {
@@ -28,23 +29,24 @@ const getPaymentById = (id, callback) => {
   });
 };
 
-// Fungsi untuk mengupdate payment
-const updatePayment = (id, reservationId, amount, status, paidAt, callback) => {
+// Update data pembayaran
+const updatePayment = (id, payment, callback) => {
+  const { reservation_id, amount, status, paid_at } = payment;
   const sql = `
     UPDATE payments
     SET reservation_id = ?, amount = ?, status = ?, paid_at = ?
     WHERE id = ?
   `;
-  const params = [reservationId, amount, status, paidAt, id];
-  db.run(sql, params, function (err) {
+  const params = [reservation_id, amount, status, paid_at, id];
+  db.run(sql, params, (err) => {
     callback(err);
   });
 };
 
-// Fungsi untuk menghapus payment
+// Hapus pembayaran
 const deletePayment = (id, callback) => {
   const sql = "DELETE FROM payments WHERE id = ?";
-  db.run(sql, [id], function (err) {
+  db.run(sql, [id], (err) => {
     callback(err);
   });
 };
