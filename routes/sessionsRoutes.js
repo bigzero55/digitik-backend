69ea93e09b732fe65bb37456d485850a1df85633
Abdul Rojak <122954244+bigzero55@users.noreply.github.com>
2024-09-11 09:12:56 +0700

    update routes/sessionsRoutes.js

--- routes/sessionsRoutes.js
+++ routes/sessionsRoutes.js
@@ -2,11 +2,19 @@ const express = require("express");
 const router = express.Router();
 const sessionsController = require("../controllers/sessionsController");
 
-// Sessions routes
+// Route untuk mendapatkan semua sesi
 router.get("/", sessionsController.getAllSessions);
+
+// Route untuk mendapatkan sesi berdasarkan ID
 router.get("/:id", sessionsController.getSessionById);
+
+// Route untuk membuat sesi baru
 router.post("/", sessionsController.createSession);
+
+// Route untuk memperbarui sesi berdasarkan ID
 router.put("/:id", sessionsController.updateSession);
+
+// Route untuk menghapus sesi dan data scanned yang terkait berdasarkan ID sesi
 router.delete("/:id", sessionsController.deleteSession);
 
 module.exports = router;
