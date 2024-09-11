05d1c67a39048e66b6351b9c15e65c82c9bd3ebf
Abdul Rojak <122954244+bigzero55@users.noreply.github.com>
2024-09-11 09:27:39 +0700

    update routes/scannedRoutes.js

--- routes/scannedRoutes.js
+++ routes/scannedRoutes.js
@@ -2,11 +2,14 @@ const express = require("express");
 const router = express.Router();
 const scannedController = require("../controllers/scannedController");
 
-// Scanned routes
 router.get("/", scannedController.getAllScanned);
+
 router.get("/:id", scannedController.getScannedById);
+
 router.post("/", scannedController.createScanned);
+
 router.put("/:id", scannedController.updateScanned);
+
 router.delete("/:id", scannedController.deleteScanned);
 
 module.exports = router;
