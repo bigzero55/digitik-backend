c32c532d886c985031b0f768022f65111c0d2236
Abdul Rojak <122954244+bigzero55@users.noreply.github.com>
2024-09-13 11:51:09 +0700

    update middlewares/authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,25 +1,22 @@
 const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
-  // Cek apakah header Authorization ada
   const authHeader = req.header("Authorization");
 
   if (!authHeader) {
     return res.status(401).json({ message: "No Authorization" });
   }
-
-  // Pastikan token dalam format yang benar "Bearer <token>"
-  const token = authHeader.split(" ")[1]; // Ambil token setelah kata "Bearer"
+  
+  const token = authHeader.split(" ")[1];
 
   if (!token) {
     return res.status(401).json({ message: "No token, authorization denied" });
   }
 
   try {
-    // Verifikasi token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // Simpan user info dari token
-    next(); // Lanjutkan ke route berikutnya
+    req.user = decoded; 
+    next();
   } catch (err) {
     res.status(401).json({ message: "Token is not valid" });
   }
