c9585e5c93fd381da0d7cf6eb94fe949e64be532
Abdul Rojak <122954244+bigzero55@users.noreply.github.com>
2024-09-20 04:57:26 +0700

    update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const HOST = process.env.HOST;
 const PORT = process.env.PORT;
 const initializeDatabase = require("./models/initializeDatabase");
 
-initializeDatabase();
 app.listen(PORT, () => {
   console.log(`Server running on http://${HOST}:${PORT}`);
 });
