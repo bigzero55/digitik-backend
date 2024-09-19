12f10f7075065e28e919e1dfcce50785930c0ef0
Abdul Rojak <122954244+bigzero55@users.noreply.github.com>
2024-09-20 05:01:07 +0700

    update models/db.js

--- models/db.js
+++ models/db.js
@@ -4,14 +4,12 @@ const mysql = require('mysql2');
 // Membuat koneksi ke MySQL
 const db = mysql.createPool(process.env.DB_URI);
     
-    db.getConnection(function (err, connection) {
+db.getConnection(function (err, connection) {
   if (err instanceof Error) {
     console.log(err);
     return;
   }
-
   console.log("Database terhubung")
-
 });
 
 module.exports = db;
