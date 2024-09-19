908765a9b953e73bb5bf63bfa9ddf52ba219c188
Abdul Rojak <122954244+bigzero55@users.noreply.github.com>
2024-09-20 05:05:25 +0700

    update models/usersModel.js

--- models/usersModel.js
+++ models/usersModel.js
@@ -3,46 +3,71 @@ const db = require("./db");
 // Tambah user baru
 const addUser = (user, callback) => {
   const { username, email, password, full_name } = user;
-  const sql =
+  const sql = 
     "INSERT INTO users (username, email, password, full_name) VALUES (?, ?, ?, ?)";
   const params = [username, email, password, full_name];
-  db.run(sql, params, function (err) {
-    callback(err, this.lastID);
+  
+  db.execute(sql, params, (err, results) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, results.insertId);
+    }
   });
 };
 
 // Dapatkan semua users
 const getAllUsers = (callback) => {
   const sql = "SELECT * FROM users";
-  db.all(sql, [], (err, rows) => {
-    callback(err, rows);
+  
+  db.execute(sql, [], (err, results) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, results);
+    }
   });
 };
 
 // Dapatkan user berdasarkan ID
 const getUserById = (id, callback) => {
   const sql = "SELECT * FROM users WHERE id = ?";
-  db.get(sql, [id], (err, row) => {
-    callback(err, row);
+  
+  db.execute(sql, [id], (err, results) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, results[0]);
+    }
   });
 };
 
 // Update data user
 const updateUser = (id, user, callback) => {
   const { username, password, full_name } = user;
-  const sql =
+  const sql = 
     "UPDATE users SET username = ?, password = ?, full_name = ? WHERE id = ?";
   const params = [username, password, full_name, id];
-  db.run(sql, params, (err) => {
-    callback(err);
+  
+  db.execute(sql, params, (err, results) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null);
+    }
   });
 };
 
 // Hapus user berdasarkan ID
 const deleteUser = (id, callback) => {
   const sql = "DELETE FROM users WHERE id = ?";
-  db.run(sql, [id], (err) => {
-    callback(err);
+  
+  db.execute(sql, [id], (err, results) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null);
+    }
   });
 };
 
